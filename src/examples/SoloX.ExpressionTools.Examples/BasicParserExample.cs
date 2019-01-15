f8a45e19bff19542f2ff903f1bdea7ca76085e49
Xavier Solau <xaviersolau@yahoo.fr>
2019-01-16 00:48:22 +0100

    add how to use examples project. Updated README.md with basic decription of the project.

---
+++ src/examples/SoloX.ExpressionTools.Examples/BasicParserExample.cs
@@ -0,0 +1,57 @@
+﻿// ----------------------------------------------------------------------
+// <copyright file="BasicParserExample.cs" company="SoloX Software">
+// Copyright (c) SoloX Software. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// ----------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using SoloX.ExpressionTools.Parser.Impl;
+using SoloX.ExpressionTools.Parser.Impl.Resolver;
+
+namespace SoloX.ExpressionTools.Examples
+{
+    /// <summary>
+    /// This class shows how to use the ExpressionParser.
+    /// </summary>
+    public static class BasicParserExample
+    {
+        /// <summary>
+        /// Let's parse a self described lambda expression like "(int x) => x + 1".
+        /// </summary>
+        public static void ParseASimpleLambda()
+        {
+            // Set the expression to parse
+            var expToParse = "(int x) => x + 1";
+
+            // We need to create the parser.
+            var expressionParser = new ExpressionParser();
+
+            // We can just parse the expression.
+            var lambdaExpression = expressionParser.Parse(expToParse);
+
+            // Or we can parse the expression specifying the type of the lambda expression.
+            var expression = expressionParser.Parse<Func<int, int>>(expToParse);
+        }
+
+        /// <summary>
+        /// Let's parse a lambda expression like "x => x + 1" using a IParameterTypeResolver.
+        /// </summary>
+        public static void ParseASimpleLambdaWithAParameterTypeResolver()
+        {
+            // Set the expression to parse
+            var expToParse = "x => x + 1";
+
+            // We need to create the parser.
+            var expressionParser = new ExpressionParser(
+                parameterTypeResolver: new DictionaryParameterTypeResolver(new Dictionary<string, Type>()
+                {
+                    { "x", typeof(int) },
+                }));
+
+            // We can just parse the expression.
+            var expression = expressionParser.Parse<Func<int, int>>(expToParse);
+        }
+    }
+}
