4898e407957a8dd7e23ec148482d596c08a10bbc
Xavier Solau <xaviersolau@yahoo.fr>
2021-05-17 02:06:24 +0200

    Add SingleInliner.

---
+++ src/libs/SoloX.ExpressionTools.Transform/Impl/SingleInliner.cs
@@ -0,0 +1,30 @@
+﻿// ----------------------------------------------------------------------
+// <copyright file="SingleInliner.cs" company="Xavier Solau">
+// Copyright © 2019 Xavier Solau.
+// Licensed under the MIT license.
+// See LICENSE file in the project root for full license information.
+// </copyright>
+// ----------------------------------------------------------------------
+
+using SoloX.ExpressionTools.Transform.Impl.Resolver;
+using System;
+using System.Linq.Expressions;
+
+namespace SoloX.ExpressionTools.Transform.Impl
+{
+    /// <inheritdoc />
+    public class SingleInliner : ISingleInliner
+    {
+        /// <inheritdoc />
+        public Expression<Func<TIn, TOut>> Inline<TIn, TInter, TOut>(
+            Expression<Func<TIn, TInter>> expression1,
+            Expression<Func<TInter, TOut>> expression2)
+        {
+            var parameterResolver = new SingleResolver<TIn, TInter>(expression1);
+
+            var pi = new ExpressionInliner(parameterResolver);
+
+            return pi.Amend<Func<TInter, TOut>, Func<TIn, TOut>>(expression2);
+        }
+    }
+}
