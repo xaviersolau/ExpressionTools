e03db4d39f3a390d0a69f8ea8d0dc57982c02456
Xavier Solau <xaviersolau@yahoo.fr>
2021-05-18 01:45:42 +0200

    Add Lambda tests.

--- src/tests/SoloX.ExpressionTools.Transform.UTest/SingleParameterInlinerTest.cs
+++ src/tests/SoloX.ExpressionTools.Transform.UTest/SingleParameterInlinerTest.cs
@@ -7,6 +7,8 @@
 // ----------------------------------------------------------------------
 
 using SoloX.ExpressionTools.Transform.Impl;
+using System;
+using System.Linq.Expressions;
 using Xunit;
 
 namespace SoloX.ExpressionTools.Transform.UTest
@@ -23,6 +25,27 @@ namespace SoloX.ExpressionTools.Transform.UTest
             Assert.NotNull(resultingExp);
 
             var func = resultingExp.Compile();
+
+            Assert.True(func(1000));
+            Assert.False(func(10));
+        }
+
+        [Fact(DisplayName = "It must in-line a single parameter lambda expression")]
+        public void SingleParameterInLinerLambdaTest()
+        {
+            var pi = new SingleParameterInliner();
+
+            Expression<Func<double, bool>> expToAmend = x => x > 1d;
+            Expression<Func<int, double>> expToInline = i => i * 0.01d;
+
+            var lambdaToAmend = (LambdaExpression)expToAmend;
+            var lambdaToInline = (LambdaExpression)expToInline;
+
+            var resultingExp = pi.Amend(lambdaToInline, lambdaToAmend);
+
+            Assert.NotNull(resultingExp);
+
+            var func = (Func<int, bool>)resultingExp.Compile();
 
             Assert.True(func(1000));
             Assert.False(func(10));
