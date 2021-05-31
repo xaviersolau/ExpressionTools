175c09cdd139b802dc1f04781b9b445a4d96078c
Xavier Solau <xaviersolau@yahoo.fr>
2021-05-31 22:48:15 +0200

    Add ConstantInliner

---
+++ src/libs/SoloX.ExpressionTools.Transform/IConstantInliner.cs
@@ -0,0 +1,33 @@
+﻿// ----------------------------------------------------------------------
+// <copyright file="IConstantInliner.cs" company="Xavier Solau">
+// Copyright © 2019 Xavier Solau.
+// Licensed under the MIT license.
+// See LICENSE file in the project root for full license information.
+// </copyright>
+// ----------------------------------------------------------------------
+
+using System.Linq.Expressions;
+
+namespace SoloX.ExpressionTools.Transform
+{
+    /// <summary>
+    /// Constant expression in-liner to resolve external variable references as constant expressions.
+    /// </summary>
+    public interface IConstantInliner
+    {
+        /// <summary>
+        /// Replace external variable use in the given expression by constant expression.
+        /// </summary>
+        /// <typeparam name="TDelegate">Delegate type of the lambda expression to convert.</typeparam>
+        /// <param name="expression">The expression in witch variables must be replaced.</param>
+        /// <returns>The resulting amended lambda expression.</returns>
+        Expression<TDelegate> Amend<TDelegate>(Expression<TDelegate> expression);
+
+        /// <summary>
+        /// Replace external variable use in the given expression by constant expression.
+        /// </summary>
+        /// <param name="expression">The expression in witch parameters must be replaced.</param>
+        /// <returns>The resulting amended abstract lambda expression.</returns>
+        LambdaExpression Amend(LambdaExpression expression);
+    }
+}
---
+++ src/libs/SoloX.ExpressionTools.Transform/Impl/ConstantInliner.cs
@@ -0,0 +1,31 @@
+﻿// ----------------------------------------------------------------------
+// <copyright file="ConstantInliner.cs" company="Xavier Solau">
+// Copyright © 2019 Xavier Solau.
+// Licensed under the MIT license.
+// See LICENSE file in the project root for full license information.
+// </copyright>
+// ----------------------------------------------------------------------
+
+using SoloX.ExpressionTools.Transform.Impl.Visitor;
+using System.Linq.Expressions;
+
+namespace SoloX.ExpressionTools.Transform.Impl
+{
+    /// <inheritdoc/>
+    public class ConstantInliner : IConstantInliner
+    {
+        /// <inheritdoc/>
+        public Expression<TDelegate> Amend<TDelegate>(Expression<TDelegate> expression)
+        {
+            var inlinerVisitor = new ConstantVisitor();
+            return (Expression<TDelegate>)inlinerVisitor.Visit(expression);
+        }
+
+        /// <inheritdoc/>
+        public LambdaExpression Amend(LambdaExpression expression)
+        {
+            var inlinerVisitor = new ConstantVisitor();
+            return (LambdaExpression)inlinerVisitor.Visit(expression);
+        }
+    }
+}
