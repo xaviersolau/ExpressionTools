79d801cc40f7d7628723b726666e1d396654a988
Xavier Solau <xaviersolau@yahoo.fr>
2021-05-31 22:48:15 +0200

    Add ConstantInliner

---
+++ src/libs/SoloX.ExpressionTools.Transform/Impl/Visitor/ConstantVisitor.cs
@@ -0,0 +1,46 @@
+﻿// ----------------------------------------------------------------------
+// <copyright file="ConstantVisitor.cs" company="Xavier Solau">
+// Copyright © 2019 Xavier Solau.
+// Licensed under the MIT license.
+// See LICENSE file in the project root for full license information.
+// </copyright>
+// ----------------------------------------------------------------------
+
+using System;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace SoloX.ExpressionTools.Transform.Impl.Visitor
+{
+    /// <summary>
+    /// Evaluate all constant node.
+    /// </summary>
+    internal class ConstantVisitor : ExpressionVisitor
+    {
+        /// <inheritdoc/>
+        public override Expression Visit(Expression node)
+        {
+            return base.Visit(node);
+        }
+
+        /// <inheritdoc/>
+        protected override Expression VisitMember(MemberExpression node)
+        {
+            if (node == null)
+            {
+                throw new ArgumentNullException(nameof(node));
+            }
+
+            if (node.Expression is ConstantExpression constNode && node.Member is FieldInfo fieldInfo)
+            {
+                var constNodeValue = constNode.Value;
+
+                var value = fieldInfo.GetValue(constNodeValue);
+
+                return Expression.Constant(value);
+            }
+
+            return base.VisitMember(node);
+        }
+    }
+}
---
+++ src/tests/SoloX.ExpressionTools.Transform.UTest/ConstResolverTest.cs
@@ -0,0 +1,39 @@
+﻿// ----------------------------------------------------------------------
+// <copyright file="ConstResolverTest.cs" company="Xavier Solau">
+// Copyright © 2019 Xavier Solau.
+// Licensed under the MIT license.
+// See LICENSE file in the project root for full license information.
+// </copyright>
+// ----------------------------------------------------------------------
+
+using SoloX.ExpressionTools.Transform.Impl;
+using System;
+using System.Linq.Expressions;
+using Xunit;
+
+namespace SoloX.ExpressionTools.Transform.UTest
+{
+    public class ConstResolverTest
+    {
+        [Fact]
+        public void IsShouldConvertExternalVariableAsConst()
+        {
+            var inliner = new ConstantInliner();
+
+            var externalValue = 0.01d;
+
+            Expression<Func<double, double>> expToInline = i => i * externalValue;
+
+            var exp = inliner.Amend(expToInline);
+
+            externalValue = 0.1d;
+
+            var func = exp.Compile();
+
+            Assert.Equal(100d, func(10000));
+
+            Expression<Func<double, double>> expectedExp = i => i * 0.01d;
+            Assert.Equal(expectedExp.ToString(), exp.ToString());
+        }
+    }
+}
