a7f7ad43bbd75392b3d85230f0b3286b998e7b0e
Xavier Solau <xaviersolau@yahoo.fr>
2021-09-07 00:43:51 +0200

    Add NameSpaceTypeNameResolver.

---
+++ src/libs/SoloX.ExpressionTools.Parser/Impl/Resolver/NameSpaceTypeNameResolver.cs
@@ -0,0 +1,51 @@
+﻿// ----------------------------------------------------------------------
+// <copyright file="NameSpaceTypeNameResolver.cs" company="Xavier Solau">
+// Copyright © 2019 Xavier Solau.
+// Licensed under the MIT license.
+// See LICENSE file in the project root for full license information.
+// </copyright>
+// ----------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+
+namespace SoloX.ExpressionTools.Parser.Impl.Resolver
+{
+    /// <summary>
+    /// ITypeNameResolver implementation that resolve a type name given a nameSpace list.
+    /// </summary>
+    public class NameSpaceTypeNameResolver : ITypeNameResolver
+    {
+        private readonly IEnumerable<string> namespaces;
+
+        /// <summary>
+        /// Setup the NameSpace type resolver.
+        /// </summary>
+        /// <param name="namespaces"></param>
+        public NameSpaceTypeNameResolver(IEnumerable<string> namespaces)
+        {
+            this.namespaces = namespaces;
+        }
+
+        /// <inheritdoc/>
+        public Type ResolveTypeName(string typeName)
+        {
+            var type = Type.GetType(typeName);
+            if (type != null)
+            {
+                return type;
+            }
+
+            foreach (var nameSpace in this.namespaces)
+            {
+                type = Type.GetType($"{nameSpace}.{typeName}");
+                if (type != null)
+                {
+                    return type;
+                }
+            }
+
+            return null;
+        }
+    }
+}
---
+++ src/tests/SoloX.ExpressionTools.Parser.UTest/Resolver/NameSpaceTypeNameResolverTest.cs
@@ -0,0 +1,37 @@
+﻿// ----------------------------------------------------------------------
+// <copyright file="NameSpaceTypeNameResolverTest.cs" company="Xavier Solau">
+// Copyright © 2019 Xavier Solau.
+// Licensed under the MIT license.
+// See LICENSE file in the project root for full license information.
+// </copyright>
+// ----------------------------------------------------------------------
+
+using SoloX.ExpressionTools.Parser.Impl.Resolver;
+using System;
+using Xunit;
+
+namespace SoloX.ExpressionTools.Parser.UTest.Resolver
+{
+    public class NameSpaceTypeNameResolverTest
+    {
+        [Theory(DisplayName = "It must resolve a type name")]
+        [InlineData(typeof(DateTime), "System", nameof(DateTime), true)]
+        [InlineData(typeof(DateTime), "System", "System.DateTime", true)]
+        [InlineData(typeof(DateTime), "System", "NoMath", false)]
+        public void ResolveTypeNameTest(
+            Type type, string nameSpace, string lookupName, bool expectedMatch)
+        {
+            var resolver = new NameSpaceTypeNameResolver(new[] { nameSpace });
+
+            var resolvedType = resolver.ResolveTypeName(lookupName);
+            if (expectedMatch)
+            {
+                Assert.Same(type, resolvedType);
+            }
+            else
+            {
+                Assert.Null(resolvedType);
+            }
+        }
+    }
+}
