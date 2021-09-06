f5e0db02a8be69e496b50a1e38ae068556d7afd2
Xavier Solau <xaviersolau@yahoo.fr>
2021-09-07 01:14:12 +0200

    Add a SingleParameterTypeResolver.

---
+++ src/libs/SoloX.ExpressionTools.Parser/Impl/Resolver/SingleParameterTypeResolver.cs
@@ -0,0 +1,45 @@
+﻿// ----------------------------------------------------------------------
+// <copyright file="SingleParameterTypeResolver.cs" company="Xavier Solau">
+// Copyright © 2019 Xavier Solau.
+// Licensed under the MIT license.
+// See LICENSE file in the project root for full license information.
+// </copyright>
+// ----------------------------------------------------------------------
+
+using System;
+
+namespace SoloX.ExpressionTools.Parser.Impl.Resolver
+{
+    /// <summary>
+    /// IParameterTypeResolver implementation using a single type to resolve a parameter type
+    /// given a parameter name.
+    /// </summary>
+    public class SingleParameterTypeResolver : IParameterTypeResolver
+    {
+        private readonly Type typeToResolve;
+        private string parameterName;
+
+        /// <summary>
+        /// Setup the single parameter type resolver.
+        /// </summary>
+        /// <param name="typeToResolve">Type to resolve.</param>
+        /// <param name="parameterName">Parameter name to match.</param>
+        public SingleParameterTypeResolver(Type typeToResolve, string parameterName = null)
+        {
+            this.typeToResolve = typeToResolve;
+            this.parameterName = parameterName;
+        }
+
+        ///<inheritdoc/>
+        public Type ResolveType(string parameterName)
+        {
+            if (this.parameterName != null && !this.parameterName.Equals(parameterName, StringComparison.Ordinal))
+            {
+                throw new NotSupportedException($"Multiple parameter detected registered: {this.parameterName} new: {parameterName}");
+            }
+
+            this.parameterName = parameterName;
+            return this.typeToResolve;
+        }
+    }
+}
---
+++ src/tests/SoloX.ExpressionTools.Parser.UTest/Resolver/SingleParameterTypeResolverTest.cs
@@ -0,0 +1,52 @@
+﻿// ----------------------------------------------------------------------
+// <copyright file="SingleParameterTypeResolverTest.cs" company="Xavier Solau">
+// Copyright © 2019 Xavier Solau.
+// Licensed under the MIT license.
+// See LICENSE file in the project root for full license information.
+// </copyright>
+// ----------------------------------------------------------------------
+
+using FluentAssertions;
+using SoloX.ExpressionTools.Parser.Impl.Resolver;
+using System;
+using Xunit;
+
+namespace SoloX.ExpressionTools.Parser.UTest.Resolver
+{
+    public class SingleParameterTypeResolverTest
+    {
+        [Theory]
+        [InlineData(typeof(string))]
+        [InlineData(typeof(DateTime))]
+        public void ItShouldResolveTheParameterName(Type typeToResolve)
+        {
+            var resolver = new SingleParameterTypeResolver(typeToResolve);
+
+            var resolvedType = resolver.ResolveType("any");
+
+            resolvedType.Should().Be(typeToResolve);
+        }
+
+        [Theory]
+        [InlineData("a", "a", true)]
+        [InlineData("a", "b", false)]
+        public void ItShouldResolveTheGivenParameterName(string name, string nameToResolve, bool shouldResolve)
+        {
+            var resolver = new SingleParameterTypeResolver(typeof(string), name);
+
+
+            if (shouldResolve)
+            {
+                var resolvedType = resolver.ResolveType(nameToResolve);
+                resolvedType.Should().Be(typeof(string));
+            }
+            else
+            {
+                Assert.Throws<NotSupportedException>(() =>
+                {
+                    resolver.ResolveType(nameToResolve);
+                });
+            }
+        }
+    }
+}
